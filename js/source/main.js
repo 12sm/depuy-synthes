50809a153b88ed0a94fe38ba9ce167a839d10b66
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-12-02 18:30:36 -0600

    adds slider

--- js/source/main.js
+++ js/source/main.js
@@ -4,17 +4,8 @@
   $(document).ready(initialize);
 
   function initialize(){
-    $('#true').click(changeBG);
-  }
-
-  function changeBG(){
-    if(document.body.style.backgroundColor == 'yellow'){
-      document.body.style.backgroundColor = 'orange';
-    }else if(document.body.style.backgroundColor == 'orange'){
-      document.body.style.backgroundColor = 'blue';
-    }else{
-      document.body.style.backgroundColor = 'yellow';
-    }
+    $(".imgLiquidFill").imgLiquid();
+    $('.flexslider').flexslider();
   }
 
 })();
